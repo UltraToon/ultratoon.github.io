6976d08cb289727657b7f82f2d7969d015e8ea77
Ivan <92064752+UltraToon@users.noreply.github.com>
2025-04-25 04:03:22 -0700

    ok

--- Minecraft_Merged/kubejs/startup_scripts/registry.js
+++ Minecraft_Merged/kubejs/startup_scripts/registry.js
@@ -1,9 +1,11 @@
-// Listen to item registry event
 StartupEvents.registry('block', event => {
     event.create('ivors_wrath')
-    //.tooltip('§l§fForbidden fusion§r of the §l§5Ender Dragon§rs soul.')
-    //.tooltip('This remnant calls forth the storm that §l§4consumes worlds.')
     .displayName('§k#§r Command Block §k#')
-    .texture('minecraft:command_block')
-    //.rarity('epic')
+    .textureAll('minecraft:block/command_block_front')
+    .item(item => {
+        item
+        .tooltip('§l§fForbidden fusion§r of the §l§5Ender Dragons§r soul.')
+        .tooltip('This remnant calls forth the storm that §l§4consumes worlds.')
+        .rarity('epic')
+    })
 })
