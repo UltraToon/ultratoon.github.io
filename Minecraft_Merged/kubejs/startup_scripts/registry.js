a0d34ad3b7e544f142611f97fdba2b6544922d9c
Ivan <92064752+UltraToon@users.noreply.github.com>
2025-04-25 04:38:37 -0700

    fasdfasdf

--- Minecraft_Merged/kubejs/startup_scripts/registry.js
+++ Minecraft_Merged/kubejs/startup_scripts/registry.js
@@ -1,7 +1,15 @@
+Platform.mods.kubejs.name = ''
 StartupEvents.registry('block', event => {
     event.create('ivors_wrath')
     .displayName('§k#§r Command Block §k#')
-    .textureAll('minecraft:block/command_block_front')
+    .texture('north', 'minecraft:block/command_block_front')  // Front face
+    .texture('south', 'minecraft:block/command_block_back')   // Back face
+    .texture('east', 'minecraft:block/command_block_side')    // East side
+    .texture('west', 'minecraft:block/command_block_side')    // West side
+    .texture('up', 'minecraft:block/command_block_front')     // Top face (can be the same as front)
+    .texture('down', 'minecraft:block/command_block_side')    // Bottom face (can be the same as side)
+    .soundType('amethyst_cluster')
+    //.textureAll('minecraft:block/command_block_front')
     .item(item => {
         item
         .tooltip('§l§fForbidden fusion§r of the §l§5Ender Dragons§r soul.')
