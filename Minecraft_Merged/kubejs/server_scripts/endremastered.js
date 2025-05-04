7ffe52a3ad3e497fd3207ac84ffbc4311eceed9f
Ivan Blackburn <xploczx@gmail.com>
2025-05-04 10:46:12 -0700

    Update on Sun May  4 10:46:12 AM PDT 2025

--- Minecraft_Merged/kubejs/server_scripts/endremastered.js
+++
@@ -1,9 +0,0 @@
-//ServerEvents.recipes(event => {
-//  event.remove({ mod: 'endrem' })
-//})
-
-//ServerEvents.blockLootTables(event => {
-  // Remove any eye from End Remastered structure chests
-  //event.removeLoot({ mod: 'endrem' });
-//});
-
